3a5fd3789ff322675e04435165198a483c00399d
guibbb <guiguive810@gmail.com>
2025-11-24 19:21:48 +0100

    maj

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -41,9 +41,9 @@ function Project() {
                 <img src={mock_tt} className="project-image zoom" alt="thumbnail" />
                 <h2>Power Split Algorithm for Individual Time Trials in Road Cycling</h2>
                 <p className="subtitle">
-                    <em>Internship completed at the end of the third year of engineering school in Applied Mathematics. Supervised by Paolo Fondin, Lead Data Scientist at Team Visma Lease a Bike</em>
+                    <em>Internship completed at the end of the third year of engineering school in Applied Mathematics, supervised by Mr.Fondin, Lead Data Scientist at Team Visma Lease a Bike</em>
                 </p>
-                <p>Implemented a power split optimization algorithm (dividing the course into segments with target power values) to optimize a rider's time. Using training data or professional race data stored in .FIT files, I modeled the impact of external factors on performance.</p>
+                <p>Implemented a power split optimization algorithm (dividing the course into segments with target power values) to optimize a rider's time. Using training or race data stored in .FIT files, I modeled the impact of external factors on performance. The algorithm has been implemented in the Staminalytics platform</p>
                 <div className="skills-chips">
                     <span className="chip">Machine Learning</span>
                     <span className="chip">Differential Evolution Algorithm</span>
@@ -63,11 +63,11 @@ function Project() {
                     <>
                         <p>Main tasks included:</p>
                         <ul>
+                            <li>Using a differential evolution algorithm to implement a power split</li>
                             <li>Detecting turns from a .GPX file to model their impact on a time trial</li>
-                            <li>Developing a predictive model for how turns affect rider performance</li>
                             <li>Integrating turn impact into the optimization algorithm for power splits</li>
-                            <li>Integrating weather impact</li>
-                            <li>Researching and quantifying as many external performance factors as possible</li>
+                            <li>Incorporating the impact of weather and aerodynamics</li>
+                            <li>Researching and quantifying as many of the external performance factors as possible</li>
                         </ul>
                     </>
                 )}
@@ -76,40 +76,40 @@ function Project() {
             {/* Projet Table Tennis */}
             <div className="project">
                 <img src={mock_leb} className="project-image zoom" alt="thumbnail" />
-                <h2>French National Team Table Tennis Analysis</h2>
-                <p className="subtitle">
-                    <em>Internship completed at the end of the second year of the Bachelor's in Data Science, at the LIRIS research laboratory in Lyon, France</em>
-                </p>
-                <p>Assisted the staff of the French National Table Tennis Team before and during the Paris 2024 Olympic Games with data science–related tasks.</p>
-                <div className="skills-chips">
-                    <span className="chip">Relational Databases</span>
-                    <span className="chip">Python Programming</span>
-                    <span className="chip">R</span>
-                    <span className="chip">SQL</span>
-                </div>
+                    <h2>French National Team Table Tennis Analysis</h2>
+                    <p className="subtitle">
+                        <em>Internship completed at the end of the second year of the Bachelor's in Data Science, at the LIRIS research laboratory in Lyon, France</em>
+                    </p>
+                    <p>Assisted the staff of the French National Table Tennis Team before and during the Paris 2024 Olympic Games by performing data science–related tasks</p>
+                    <div className="skills-chips">
+                        <span className="chip">Relational Databases</span>
+                        <span className="chip">Python Programming</span>
+                        <span className="chip">R</span>
+                        <span className="chip">SQL</span>
+                    </div>
 
-                <button 
-                    className="see-more-btn" 
-                    onClick={() => toggleShowMore("tableTennis")}
-                >
-                    {showMoreStates.tableTennis ? 'See less' : 'See more'}
-                </button>
+                    <button 
+                        className="see-more-btn" 
+                        onClick={() => toggleShowMore("tableTennis")}
+                    >
+                        {showMoreStates.tableTennis ? 'See less' : 'See more'}
+                    </button>
 
-                {showMoreStates.tableTennis && (
-                    <>
-                        <p>Main tasks included:</p>
-                        <ul>
-                            <li>Creating and updating a relational database to store match data</li>
-                            <li>Automating video compilations using SQL queries</li>
-                            <li>Generating automated data tables for a given match</li>
-                        </ul>
-                        <p className='video_legend'>Compilation of Wang Chuqin’s short returns against Felix Lebrun during the WTT Finals Men Doha 2023 :</p>
-                        <video className="video-responsive" autoPlay loop muted>
-                            <source src={mock_tt_video} type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
-                        <p>The main challenge I overcame was understanding a sport that was completely new to me.</p>
-                    </>
+                    {showMoreStates.tableTennis && (
+                        <>
+                            <p>Main tasks included:</p>
+                            <ul>
+                                <li>Creating and updating a relational database to store match data</li>
+                                <li>Automating video compilations using SQL queries</li>
+                                <li>Generating automated data tables for a given match</li>
+                            </ul>
+                            {/* <p className='video_legend'>Compilation of Wang Chuqin’s short returns against Felix Lebrun during the WTT Finals Men Doha 2023 :</p>
+                            <video className="video-responsive" autoPlay loop muted>
+                                <source src={mock_tt_video} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video> */}
+                            <p>The main challenge I overcame was understanding a sport that was completely new to me</p>
+                        </>
                 )}
             </div>
 
@@ -126,13 +126,13 @@ function Project() {
                 <h2>Rider Potential Prediction in Road Cycling</h2>
                 <p className="subtitle">
                     <em>
-                        Ongoing project during the third year of the Bachelor's in Data Science. 
-                        Supervised by Paolo Fondin, Lead Data Scientist at Team Visma Lease a Bike
+                        Ongoing project from the third year of the Bachelor's in Data Science. 
+                        Supervised by Mr.Fondin, Lead Data Scientist at Team Visma Lease a Bike
                     </em>
                 </p>
                 <p>
                     I am creating Machine Learning models to predict a rider's potential over four years based on a physiological test in road cycling. 
-                    I use variables such as, Anaerobic Threshold, FatMax Power, Max Lactate Steady State (MLSS), VO2max, or VLamax from physiological tests 
+                    I use variables such as FatMax Power, Max Lactate Steady State (MLSS), VO2max, VO2ss, and VLamax from physiological tests 
                     to predict performance indicators that I create from race results.
                 </p>
                 <div className="skills-chips">
@@ -170,10 +170,10 @@ function Project() {
                 <img src={mock_football} className="project-image zoom" alt="thumbnail" />
                 <h2>Event Prediction Using StatsBomb Data</h2>
                 <p className="subtitle">
-                    <em>Ongoing project during the third year of the Bachelor's in Data Science</em>
+                    <em>Ongoing project from the third year of the Bachelor's in Data Science</em>
                 </p>
                 <p>
-                    I use StatsBomb Open Data in order to implement classification methods such as K-Nearest Neighbors (KNN), Linear Discriminant Analysis (LDA), and Decision Trees to predict whether a shot will be on target, whether a pass will lead to a shot shortly afterward, or whether a corner will result in a shot
+                    I use StatsBomb Open Data to implement classification methods such as K-Nearest Neighbors (KNN), Linear Discriminant Analysis (LDA), and Decision Trees to predict whether a shot will be on target, whether a pass will lead to a shot shortly afterward, or whether a corner will result in a shot
                 </p>
                 <div className="skills-chips">
                     <span className="chip">K-Nearest Neighbors (KNN)</span>
@@ -251,7 +251,7 @@ function Project() {
           <p className="subtitle">
             <em>Project completed in 2nd year of the Bachelor's in Data Science</em>
           </p>
-          <p>Described and forecasted US domestic air traffic for two weeks using time series modeling with R (135,000 records from 2015).</p>
+          <p>Described and forecasted US domestic air traffic for two weeks using time series modeling with R (135,000 records from 2015)</p>
           <div className="skills-chips">
             <span className="chip">Time Series Modeling</span>
             <span className="chip">R Programming</span>
@@ -283,7 +283,7 @@ function Project() {
                     <>
                         <p>Main tasks included:</p>
                         <ul>
-                            <li>Create several interactive sheets with filters and buttons to navigate between the sheets</li>
+                            <li>Created several interactive sheets with filters and buttons to navigate between the sheets</li>
                             <li>Set up an ETL process to process the data from CSV files</li>
                             <li>Created indicators using the DAX language</li>
                             <li>Used Power Query to process the data</li>
@@ -299,7 +299,7 @@ function Project() {
                 <em>Personal Project</em>
             </p>
             <p>
-                Created a database with fictional riders that realistically reflect real-world characteristics. To do this, I studied an existing database and the relationships between the different attributes of professional cyclists. The goal was to build an alternative, customizable database to replace the usual riders
+                Created a database with fictional riders that realistically reflect the characteristics or real riders. To do this, I studied an existing database and the relationships between the different attributes of professional cyclists. The goal was to build an alternative, customizable database to replace the usual database
             </p>
             <div className="skills-chips">
                 <span className="chip">Database</span>
@@ -315,7 +315,7 @@ function Project() {
           <p className="subtitle">
             <em>Project completed in 2nd year of the Bachelor's in Data Science</em>
           </p>
-          <p>Implemented and fed a ROLAP data warehouse in MySQL via an ETL process using Talend. The project focused on adoption data from SPA du Dauphiné to allow statistical analysis via SQL queries.</p>
+          <p>Implemented and fed a ROLAP data warehouse in MySQL via an ETL process using Talend. The project focused on adoption data from SPA du Dauphiné to allow statistical analysis via SQL queries</p>
           <div className="skills-chips">
             <span className="chip">ETL Development (Talend)</span>
             <span className="chip">Data Integration (CSV, XLS, XML)</span>
